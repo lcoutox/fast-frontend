42ee0c843d7cbcecc955c1dc9adaede2fa532551
Lucas Couto <lucas3couto@gmail.com>
2023-12-21 20:52:36 -0300

    chore: add eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { PluginOption, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    process.env.ANALYZE &&
+      (visualizer({ gzipSize: true, open: true }) as PluginOption),
+  ].filter(Boolean),
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]__[hash:8]",
+      localsConvention: null,
+    },
+  },
+});
